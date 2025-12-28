f4455b1e704526d636cca26f47e13246baddd7b3
Pawan Mohit <168399930+PawanMohit16@users.noreply.github.com>
2025-12-28 21:06:51 +0530

    2271-REARRANGE-ARRAY-ELEMENTS-BY-SIGN Stats: Time: 42 ms (70.76%), Space: 40.1 MB (94.29%) - LeetHub

--- 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.py
+++ 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.py
@@ -1,20 +1,16 @@
 class Solution:
     def rearrangeArray(self, nums: List[int]) -> List[int]:
-        odd = []
-        even = []
+        ecnt = 0
+        ocnt = 1
+        n = len(nums)
+        ans = [None] * n
 
-        for num in range(len(nums)):
-            if nums[num] > 0:
-                even.append(nums[num])
-
+        for num in nums:
+            if num > 0:
+                ans[ecnt] = num
+                ecnt += 2
             else:
-                odd.append(nums[num])
-
-        ans = []
-                
-        for i in range(len(even)):
-            ans.append(even.pop(0))
-            ans.append(odd.pop(0))
+                ans[ocnt] = num
+                ocnt += 2
 
         return ans
-        
