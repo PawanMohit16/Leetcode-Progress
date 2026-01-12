e7427e0eb5f9b7417eb69c4054b7f47035359cb5
Pawan Mohit <168399930+PawanMohit16@users.noreply.github.com>
2026-01-13 01:43:20 +0530

    0034-FIND-FIRST-AND-LAST-POSITION-OF-ELEMENT-IN-SORTED-ARRAY Stats: Time: 0 ms (100%), Space: 20.8 MB (5.51%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,32 +1,41 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        start, end = len(nums)-1, -1
+        start, end = len(nums), -1
         low, high = 0, len(nums)-1
         
-        def bs(low, high):
+        while low <= high:
+            mid = (low + high) // 2
 
-            if low <= high:
-                nonlocal start, end
-                mid = (low + high) // 2
+            if nums[mid] > target:
+                high = mid - 1
 
-                if nums[mid] > target:
-                    bs(low, mid - 1)
+            elif nums[mid] == target:
+                start = min(start, mid)
+                high = mid - 1
 
-                elif nums[mid] == target:
-                    start = min(mid, start)
-                    end = max(mid, end)
-                    bs(mid + 1, high)
-                    bs(low, mid - 1)
-                else:
-                    bs(mid + 1, high)
+            else:
+                low = mid + 1
 
+        low, high = 0, len(nums)-1
+
+        while low <= high:
+            mid = (low + high) // 2
+
+            if nums[mid] < target:
+                low = mid + 1
+            
+            elif nums[mid] == target:
+                end = max(end, mid)
+                low = mid + 1
+            
+            else:
+                high = mid - 1
 
 
         
-        bs(low, high)
-        if end == -1:
+        if start == -1 or end == -1:
             return [-1, -1]
-        
-        return [start, end]
 
+        else:
+            return [start, end]
         
