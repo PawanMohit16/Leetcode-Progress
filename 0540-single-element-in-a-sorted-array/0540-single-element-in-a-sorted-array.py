368b855eb1dc59a5d18e629951d3d0c1e3055759
Pawan Mohit <168399930+PawanMohit16@users.noreply.github.com>
2026-01-17 00:32:14 +0530

    0540-SINGLE-ELEMENT-IN-A-SORTED-ARRAY Stats: Time: 0 ms (100%), Space: 27.2 MB (9.21%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.py
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.py
@@ -8,7 +8,7 @@ class Solution:
 
             if mid % 2 == 0:
                 if mid < n-1 and nums[mid] == nums[mid + 1]:
-                    low = mid + 2
+                    low = mid + 1
                 elif mid > 0 and nums[mid] == nums[mid - 1]:
                     high = mid - 1
                 else:
