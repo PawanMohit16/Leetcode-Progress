cc6263e97d863d5e4801d5fb0753ce33f816615f
Pawan Mohit <168399930+PawanMohit16@users.noreply.github.com>
2026-07-02 01:00:05 +0530

    0118-PASCALS-TRIANGLE Stats: Time: 0 ms (100%), Space: 19.3 MB (68.24%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.py
+++ 0118-pascals-triangle/0118-pascals-triangle.py
@@ -10,12 +10,8 @@ class Solution:
 
             arr.append(temp)
 
-        if n > 2:
-            for i in range(2, n):
-                print(len(arr[i]))
-                for j in range(len(arr[i-1])-1):
-                    arr[i][j+1] = arr[i-1][j]+arr[i-1][j+1]
-
-
+        for i in range(2, n):
+            for j in range(1, len(arr[i-1])):
+                arr[i][j] = arr[i-1][j-1]+arr[i-1][j]
 
         return arr
